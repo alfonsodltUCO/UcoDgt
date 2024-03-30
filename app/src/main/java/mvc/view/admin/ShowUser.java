8333327f2b1a87a18fc8b65479e1e9c9cac7e3b0
alfonso de la torre <alfonso.delatorre@sepalo.es>
2024-03-30 23:55:39 +0100

    modifying existing

--- app/src/main/java/mvc/view/admin/ShowUser.java
+++ app/src/main/java/mvc/view/admin/ShowUser.java
@@ -3,20 +3,64 @@ package mvc.view.admin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ucodgt.R;
 
+import java.text.SimpleDateFormat;
+
+import mvc.model.business.user.client.ClientDTO;
 import mvc.model.business.user.worker.WorkerDTO;
 
+
 public class ShowUser extends AppCompatActivity {
+    TextView name,surname,email,numberofworker_licencepoints,birth,dni;
+    String strDate;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_user);
+
         Intent intentFound=getIntent();
-        WorkerDTO worker = (WorkerDTO) getIntent().getSerializableExtra("worker");
+        String type=intentFound.getStringExtra("type");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        name=findViewById(R.id.textViewFoundName);
+        birth=findViewById(R.id.textViewFoundDateBirth);
+        surname=findViewById(R.id.textViewFoundSurname);
+        email=findViewById(R.id.textViewFoundEmail);
+        dni=findViewById(R.id.textViewFoundDni);
+        numberofworker_licencepoints=findViewById(R.id.textViewFoundLicencePoints_numberworker);
+
+
+        if(type.equals("worker")){
+
+            WorkerDTO worker = (WorkerDTO) getIntent().getSerializableExtra("worker");
+            name.setText("name= "+worker.getName());
+            surname.setText("surname= "+worker.getSurname());
+            email.setText("email= "+worker.getEmail());
+            numberofworker_licencepoints.setText("worker number= "+worker.getNumberOfWorker().toString());
+            dni.setText("dni= "+worker.getDni());
+            strDate= formatter.format(worker.getAge());
+            birth.setText("birth= "+strDate);
+
+        }else{
+
+            ClientDTO client = (ClientDTO) getIntent().getSerializableExtra("client");
+            name.setText("name= "+client.getName());
+            surname.setText("surname= "+client.getSurname());
+            email.setText("email= "+client.getEmail());
+            numberofworker_licencepoints.setText("licence points= "+client.getLicencepoints().toString());
+            dni.setText("dni= "+client.getDni());
+            strDate= formatter.format(client.getAge());
+            birth.setText("birth= "+strDate);
+
+        }
+
+
     }
 }
