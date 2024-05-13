597f8d55ab961d22b87b13d7df6a6acf781ac939
alfonso de la torre <alfonso.delatorre@sepalo.es>
2024-05-13 19:36:12 +0200

    send email after a penalty

--- app/src/main/java/com/uco/ucodgt/mvc/controller/admin/penalty/CheckPenaltyToAdd.java
+++ app/src/main/java/com/uco/ucodgt/mvc/controller/admin/penalty/CheckPenaltyToAdd.java
@@ -25,7 +25,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-import com.uco.ucodgt.mvc.emailsender;
 import com.uco.ucodgt.mvc.model.business.penalty.ManagerPenalty;
 import com.uco.ucodgt.mvc.model.business.penalty.PenaltyDTO;
 import com.uco.ucodgt.mvc.model.business.penalty.list.ListPenaltyDTO;
